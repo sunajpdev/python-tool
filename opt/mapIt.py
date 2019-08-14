d771a7ad6435ceddca6778defadfb4685cf5c0dc
sunajpdev <sunajp.dev@gmail.com>
2019-08-14 11:39:49 +0900

    ref func

--- opt/mapIt.py
+++ opt/mapIt.py
@@ -1,3 +1,4 @@
+# coding:utf-8
 #! python3
 # mapIt.py - コマンドラインやクリップポードに指定した住所の地図を開く
 
@@ -6,37 +7,58 @@ import sys
 import pyperclip
 import urllib.parse
 
-# 初期値
-site = 'google'
-param = ''
+# # 乗換案内 start: 開始駅 to:到着駅 
+# def norikae(start, to):
+#   return 'https://transit.yahoo.co.jp/main/top?from=' + start +'&to=' + to
 
-# 第一引数がない場合はエラー
-if len(sys.argv) < 2:
-  print("ERROR: パラメータ不足")
-  print("第一引数に調査対象を指定 [map, google, youtube]")
-  sys.exit()
+# サイトごとにURLとパラメータをセットした文字列を返す
+def createUrl(site, argv):
+  # 検索パラメータ取得
+  param = paramArgv(argv)
 
-if len(sys.argv) > 2:
-  # 第一引数をサイトパラメータとして取得
-  site = sys.argv[1]
+  # 第一引数でサイトのURLを変更する
+  if site == 'gmap':
+    url = 'https://www.google.com/maps/search/'+ param
+  elif site == 'youtube':
+    url = 'https://www.youtube.com/results?search_query=' + param
+  elif site == 'ggl':
+    url = 'https://www.google.com/search?q=' + param
+  elif site == 'gphoto':
+    url = 'https://www.google.com/search?q=' + param + '&tbm=isch'
+  else:
+    print('ERROR: Site Not Found')
+    sys.exit()
+  
+  return url
 
-  # コマンドラインから住所取得する
-  param = ' ' . join(sys.argv[2:])
-else:
-  # クリップポードから住所を取得する
-  param = pyperclip.paste()
+# パラメータより検索先サイトを返す。ない場合はエラーを出して終了
+def siteName(argv):
+  # 第一引数がない場合はエラー
+  if len(argv) < 2:
+    print("ERROR: パラメータ不足")
+    sys.exit()
+  else:
+    return argv[1]
 
-# 第一引数でサイトのURLを変更する
-if site == 'map':
-  base_url = 'https://www.google.com/maps/search/'
-elif site == 'youtube':
-  base_url = 'https://www.youtube.com/results?search_query='
-elif site == 'google':
-  base_url = 'https://www.google.com/search?q='
-else:
-  base_url = 'https://www.google.com/search?q='
+# パラメータの第二パラメータ以降、ない場合はクリップボードから検索用文字列を返す
+def paramArgv(argv):
+  if len(argv) > 2:
+    # 第一引数をサイトパラメータとして取得
+    site = argv[1]  
+    # コマンドラインから住所取得する
+    param = ' ' . join(argv[2:])
+  else:
+    # クリップポードから住所を取得する
+    param = pyperclip.paste()
 
-# 日本語の可能性があるため address エンコードする
-url = base_url + urllib.parse.quote(param)
-webbrowser.open(url)
+  # 日本語の可能性があるため address エンコードする  
+  param = urllib.parse.quote(param)
+  return param
 
+
+if __name__ == "__main__":
+  # 検索サイトを取得
+  site = siteName(sys.argv)
+  # urlを生成
+  url = createUrl(site, sys.argv)
+  webbrowser.open(url)
